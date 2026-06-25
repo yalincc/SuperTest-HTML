9454fc54af2b5c884fbc3bf50522ce02436da5be
yalincc <22169292@qq.com>
2026-06-25 14:03:01 +0800

    fix: add base path and switch to HashRouter for GitHub Pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { HashRouter, Routes, Route } from 'react-router-dom'
 import AppLayout from './components/layout/AppLayout'
 import HomePage from './pages/HomePage'
 import SubjectPage from './pages/SubjectPage'
@@ -10,7 +10,7 @@ import StatsPage from './pages/StatsPage'
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route element={<AppLayout />}>
           <Route path="/" element={<HomePage />} />
@@ -23,7 +23,7 @@ function App() {
           <Route path="/stats" element={<StatsPage />} />
         </Route>
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   )
 }
 
